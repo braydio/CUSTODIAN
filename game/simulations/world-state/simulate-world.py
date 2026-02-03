6d1e07e5f0a03a2f51a426fbf97ad06cf1d2a505
braydio <chaffee.brayden@gmail.com>
2026-02-02 23:52:42 -0500

    wolrd sim

--- game/simulations/world-state/simulate-world.py
+++ game/simulations/world-state/simulate-world.py
@@ -1,5 +1,9 @@
+import os
+
 from world_state.simulation import simulate_world
 
 
 if __name__ == "__main__":
-    simulate_world()
+    tick_delay = float(os.getenv("TICK_DELAY", "0.05"))
+    ticks = int(os.getenv("TICKS", "300"))
+    simulate_world(ticks=ticks, tick_delay=tick_delay)
