ce4154b148b5df67c6c147f36010bdca2466fdc3
braydio <chaffee.brayden@gmail.com>
2026-03-04 04:29:06 -0500

    Move rclone sync to root, add .godot exclusion

--- rclone-git-sync.sh
+++ rclone-git-sync.sh
@@ -7,6 +7,7 @@ echo "Syncing $REPO_DIR to G-Drive..."
 
 rclone sync "$REPO_DIR" "$G_DRIVE_REMOTE" \
   --exclude ".git/**" \
+  --exclude ".godot/**" \
   --exclude ".venv/**" \
   --exclude "__pycache__/**" \
   --progress
